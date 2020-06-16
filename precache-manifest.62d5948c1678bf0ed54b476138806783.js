06ed1d01ca2f1d0aef08684b77e6337373d7b822
Vannak <vannaksuos@gmail.com>
2020-06-15 20:08:11 -0400

    Updates

---
+++ precache-manifest.62d5948c1678bf0ed54b476138806783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265f86825ff7db243ee3e2f64bf0383c",
+    "url": "/google-books/index.html"
+  },
+  {
+    "revision": "db5d864780b369461a28",
+    "url": "/google-books/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e22d174232cdea9e63b5",
+    "url": "/google-books/static/js/2.376af5f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google-books/static/js/2.376af5f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5d864780b369461a28",
+    "url": "/google-books/static/js/main.8d8bfbc2.chunk.js"
+  },
+  {
+    "revision": "8e4019dd128d035ed7ff",
+    "url": "/google-books/static/js/runtime-main.6e29cb3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-books/static/media/logo.5d5d9eef.svg"
+  }
+]);
