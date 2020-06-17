0d676ebedc5bafe7af3d79f314073d2cfa4d39f2
Vannak <vannaksuos@gmail.com>
2020-06-17 13:25:40 -0400

    started on app.post

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
@@ -20,14 +20,27 @@ app.get("/api/books", (req, res) => {
     bookModel.find().then(savedBooks => {
         res.json(savedBooks)
     })
-}) 
- 
+});
+
+app.post("/api/books", ({ body }, res) => {
+    const book = body;
+    bookModel.save(book, (error, savedBooks) => {
+    if (error) {
+        console.log(error);
+    } else {
+        res.send(savedBooks);
+    }
+    });
+});
+
+
+
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
