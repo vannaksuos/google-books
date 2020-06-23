25423929cdf915f7bf749b9a1d6d3f969651bd1d
Vannak <vannaksuos@gmail.com>
2020-06-23 10:24:37 -0400

    adding import './App.css';

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Saved from "./components/Saved";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
+import './App.css';
+
+
 
 function App() {
   return (
